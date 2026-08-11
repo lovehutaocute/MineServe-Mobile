6654d16fd6a3e7739dac6bac4952345f9d5e0131
MC Server Manager <mc-server-manager@local>
2026-08-11 17:44:59 +0800

    chore: bump app version for Forge Java 8 fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.mineserve.mobile"
         minSdk = 26
         targetSdk = 28
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
